550963a8db8b163b0050cd3c190f2385d70fddaa
Akseli Piilola <akseli.piilola@gofore.com>
2017-10-05 16:10:31 +0300

    Added a model for bus data.

--- llb-poc/src/app/app.component.ts
+++ llb-poc/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {LlbService} from './service/llb.service';
 import {LoaderService} from './service/loader.service';
+import {VehicleData} from './model/VehicleData';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,8 @@ import {LoaderService} from './service/loader.service';
 export class AppComponent {
   title = 'app works!';
   testOk: boolean = null;
+
+  model: VehicleData;
 
   constructor(public llbService: LlbService, public loader: LoaderService) {}
 
@@ -21,8 +24,11 @@ export class AppComponent {
         }
       })
       .subscribe(res => {
-        console.log(res.json());
         const data = res.json();
+
+        this.model = data;
+
+        console.log(this.model);
 
         try {
           this.testOk = (data.BusId === 1612);
@@ -30,5 +36,13 @@ export class AppComponent {
           this.testOk = false;
         }
       });
+  }
+
+  getVehicleId(): string {
+    if (this.model != null) {
+      return this.model.BusId.toString();
+    } else {
+      return 'n/a';
+    }
   }
 }
