518fad968c4ce42fd2a3f41a2a31fdb82d233e35
unknown <tuomas.kaartoluoma@gmail.com>
2017-11-27 19:43:56 +0200

    message

--- llb-poc/src/app/menu/mock-menubuttons.ts
+++ llb-poc/src/app/menu/mock-menubuttons.ts
@@ -1,6 +1,7 @@
 import { MenuButton } from './menubutton';
 
 export const MENUBUTTONS: MenuButton[] = [
+	{ id: 0, title: 'Home'},
 	{ id: 1, title: 'General Data'},
 	{ id: 2, title: 'Location Data'},
 	{ id: 3, title: 'Engine Data' },
