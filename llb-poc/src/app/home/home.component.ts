c38c3cdb1198c095612554ba306a24e0c0506be5
Toni Linnusmaki <toni.linnusmaki@power.fi>
2017-12-17 15:50:15 +0200

    fixed modal windows and agm map

--- llb-poc/src/app/home/home.component.ts
+++ llb-poc/src/app/home/home.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {LlbService} from '../service/llb.service';
+import { ModalComponent } from 'app/modal/modal.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
+  _selectedModal: number;
   constructor(public service: LlbService) { }
 
   ngOnInit() {
+  }
+
+  showModal(modal: ModalComponent, id: number) {
+    modal.show();
+    this._selectedModal = id;
   }
 
 }
