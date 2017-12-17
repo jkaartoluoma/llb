6ace49f09304e0b6eda5ac3ef74c905639b10c4f
Toni Linnusmaki <toni.linnusmaki@power.fi>
2017-12-17 16:56:00 +0200

    location data component layout changes

--- llb-poc/src/app/location-data/location-data.component.ts
+++ llb-poc/src/app/location-data/location-data.component.ts
@@ -24,4 +24,21 @@ export class LocationDataComponent implements OnInit {
     }
   }
   
+  // 0=no mode value yet seen 1=no fix 2=2D 3=3D
+  // makes string from NMEA mode data
+  getNmeaMode(): string {
+    try {
+      if (this.llbService.data[0].mod === 1) {
+        return 'no fix';
+      } else if  (this.llbService.data[0].mod === 2) {
+        return '2D';
+      } else if (this.llbService.data[0].mod === 3) {
+        return '3D';
+      } else {
+        return 'no mode value yet';
+      }
+    } catch (e) {
+      return 'unknown';
+    }
+  }
 }
