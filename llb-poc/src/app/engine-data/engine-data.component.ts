4832414c1940e58d8e74873be3eef157c90c26bc
Toni Linnusmaki <toni.linnusmaki@power.fi>
2017-12-22 15:05:41 +0200

    added functionality behing gauges

--- llb-poc/src/app/engine-data/engine-data.component.ts
+++ llb-poc/src/app/engine-data/engine-data.component.ts
@@ -16,12 +16,28 @@ export class EngineDataComponent implements OnInit {
   constructor(public llbService: LlbService, public loader: LoaderService) { }
 
   ngOnInit() {
+    // if we want to use this gauge change this to 
+    // this.MotorRPM();
     this.randomRot();
   }
 
   randomRot():void {
     setInterval (() => {
-      this.gaugeValue = Math.random()*100
+      this.gaugeValue = Math.random()*10000
     }, 1000);
+  }
+
+  isRpmValid(): boolean {
+    return this.llbService.isLiveData() && !isNullOrUndefined(this.llbService.data[0].can.EEC1_MotorSpeed)
+  }
+
+  motorRPM(): void {
+    setInterval (() => {
+      if(this.llbService.isLiveData() && !isNullOrUndefined(this.llbService.data[0].can.EEC1_MotorSpeed)) {
+        if(this.llbService.data[0].can.EEC1_MotorSpeed !== this.gaugeValue) {
+          this.gaugeValue = this.llbService.data[0].can.EEC1_MotorSpeed;
+        }
+      } 
+    }, 200);
   }
 }
