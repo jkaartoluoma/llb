16a999b3539eac3cddc44ff6a8fbacf9a6f65f65
jkaartoluoma <joonas.kaartoluoma@gmail.com>
2017-12-22 12:24:25 +0200

    engine data into divs

--- llb-poc/src/app/engine-data/engine-data.component.ts
+++ llb-poc/src/app/engine-data/engine-data.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {LlbService} from '../service/llb.service';
 import {LoaderService} from '../service/loader.service';
+import {isNullOrUndefined} from '../../utils';
 
 @Component({
   selector: 'engine-data',
@@ -8,7 +9,9 @@ import {LoaderService} from '../service/loader.service';
   styleUrls: ['./engine-data.component.css']
 })
 export class EngineDataComponent implements OnInit {
-
+	
+  isNullOrUndefined = isNullOrUndefined;
+  
   constructor(public llbService: LlbService, public loader: LoaderService) { }
 
   ngOnInit() {
