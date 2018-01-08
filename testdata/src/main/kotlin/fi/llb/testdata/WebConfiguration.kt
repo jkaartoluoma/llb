1a41ca1dc48a2c02ebe757a8076a2deedc08e2eb
Akseli Piilola <akseli.piilola@gofore.com>
2018-01-08 16:24:51 +0200

    Lisätty cors mappings testiservulle.

---
+++ testdata/src/main/kotlin/fi/llb/testdata/WebConfiguration.kt
@@ -0,0 +1,14 @@
+package fi.llb.testdata
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@Configuration
+@EnableWebFlux
+class WebConfiguration : WebFluxConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry?) {
+        registry!!.addMapping("/**").allowedOrigins("*")
+    }
+}
