af4c2c73ab1219498122c538dff2a4e295b54681
unknown <tuomas.kaartoluoma@gmail.com>
2018-01-17 18:17:47 +0200

    desktop css

--- llb-poc/src/app/app.component.ts
+++ llb-poc/src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent implements OnInit {
   title = 'app works!';
 
   vehicleIds:number[];
+  body:HTMLElement;
 
   constructor(public llbService: LlbService, public loader: LoaderService) {}
 
@@ -20,11 +21,30 @@ export class AppComponent implements OnInit {
     this.llbService.start();
     this.getActiveVehicles();
     this.llbService.vehicleId = this.vehicleIds[0];
+    this.body = document.getElementById('body');
+    this.animateBackground();
   }
 
   getActiveVehicles() {
     this.vehicleIds = [
       1612,3008,3009,9999
     ]
+  }
+
+  animateBackground() {
+    setInterval(() => {
+      if (this.llbService.isLiveData()) {
+        if (Number(this.llbService._vehicleId) == this.llbService.data[0].BusId) {
+          this.body.style.animation = "animatedBackground 60s linear infinite";
+          if (this.llbService.data[0].can.TCO1_VehicleMotion == 1) {
+            this.body.style.animationPlayState = "running";
+          } else {
+            this.body.style.animationPlayState = "paused";
+          }
+        }
+      } else {
+        this.body.style.animationPlayState = "paused";
+      }
+    }, 500);
   }
 }
